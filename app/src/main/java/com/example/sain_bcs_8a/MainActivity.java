d7ac39960dbbc33c9ed05f6017d22b4b4b214dd4
Rana Waqas <programmingwithranawaqas@gmail.com>
2025-01-30 19:49:40 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sain_bcs_8a/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sain_bcs_8a;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // hooks
+    EditText etCnic;
+    TextView tvResult;
+    Button btnClear, btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etCnic = findViewById(R.id.etCnic);
+        tvResult = findViewById(R.id.tvResult);
+        btnClear = findViewById(R.id.btnClear);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        tvResult.setText("Mousa Baig");
+
+
+    }
+
+
+}
